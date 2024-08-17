3f0cc165220e4907d9e739542611dcb1c5ab05e6
deadpool <118651379+DocStrange31@users.noreply.github.com>
2024-08-17 12:08:56 +0530

    Completed basic layout of cards of books in library

---
+++ script.js
@@ -0,0 +1,88 @@
+const mylib=[];
+
+const addbtn=document.querySelector('#add');
+const dialog=document.querySelector('dialog');
+const form=document.querySelector('form');
+const cancelbtn=document.querySelector('.cancel');
+const submitbtn=document.querySelector('.submit');
+addbtn.addEventListener('click',()=>{
+    dialog.showModal();
+})
+cancelbtn.addEventListener('click',()=>{
+    document.querySelector('#title').value='';
+    document.querySelector('#author').value='';
+    document.querySelector('#pages').value='';
+    document.querySelector('#read').checked=false;
+    document.querySelectorAll('input').forEach((element)=>{
+        element.classList.remove('selected');
+    })
+    dialog.close();
+})
+
+form.addEventListener('keydown',(event)=>{
+    if(event.key==='Enter'){
+        event.preventDefault();
+        const newevent=new Event('click');
+        submitbtn.dispatchEvent(newevent);
+    }
+})
+
+function Book(title,author,pages,read){
+    this.title=title;
+    this.author=author;
+    this.pages=pages;
+    this.read=read;
+};
+
+document.querySelector('.submit').addEventListener('click',()=>{
+    document.querySelectorAll('input').forEach((element)=>{
+        element.classList.add('selected');
+    })
+})
+
+document.querySelector('dialog form').addEventListener('submit',(event)=>{
+    event.preventDefault();
+    let title=document.querySelector('#title').value;   
+    let author=document.querySelector('#author').value;
+    let pages=document.querySelector('#pages').value;
+    let read=document.querySelector('#read').checked;
+    mylib.push(new Book(title,author,pages,read));
+    document.querySelector('#title').value='';
+    document.querySelector('#author').value='';
+    document.querySelector('#pages').value='';
+    document.querySelector('#read').checked=false;
+    document.querySelectorAll('input').forEach((element)=>{
+        element.classList.remove('selected');
+    })
+    const div=document.createElement('div');
+    div.classList.add('card');
+    const p1=document.createElement('p');
+    p1.textContent=title;
+    const p2=document.createElement('p');
+    p2.textContent='by '+author;
+    const p3=document.createElement('p');
+    p3.textContent=pages+' Pages';
+    div.appendChild(p1);
+    div.appendChild(p2);
+    div.appendChild(p3);
+    const readbtn=document.createElement('button');
+    if(read){
+        readbtn.classList.add('read');
+        readbtn.textContent='Read';
+    } 
+    else readbtn.textContent='Not Read';
+    readbtn.setAttribute('id','read');
+    readbtn.addEventListener('click',()=>{
+        readbtn.classList.toggle('read');
+        if(readbtn.classList.contains('read')) readbtn.textContent='Read';
+        else readbtn.textContent='Not Read';
+    })
+    const remove=document.createElement('button');
+    remove.textContent='Remove';
+    remove.setAttribute('id','remove');
+    div.appendChild(readbtn);
+    div.appendChild(remove);
+    div.dataset.index=mylib.length-1;
+    document.querySelector('.cards').appendChild(div);
+    dialog.close();
+})
