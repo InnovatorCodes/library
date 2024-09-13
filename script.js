0bf10c82f935bbb399ab4e73bb6487b46ef9522b
deadpool <118651379+DocStrange31@users.noreply.github.com>
2024-09-13 19:36:50 +0530

    Added functionality to edit button Added extra border thickness for focussed input element Fixed error related to cancel button trying to submit form

--- script.js
+++ script.js
@@ -1,12 +1,17 @@
 let mylib=[];
 
+let currentedit;
+
 const addbtn=document.querySelector('#add');
-const dialog=document.querySelector('dialog');
-const editdialog=document.querySelector('dialog.edit');
-const form=document.querySelector('dialog form');
-const cancelbtn=document.querySelector('.cancel');
-const submitbtn=document.querySelector('.submit');
+const dialog=document.querySelector('dialog.add');
+const form=document.querySelector('dialog.add form');
+const cancelbtn=document.querySelector('.add .cancel');
+const submitbtn=document.querySelector('.add .submit');
 const cards=document.querySelector('.cards');
+const editdialog=document.querySelector('dialog.edit');
+const editform=document.querySelector('dialog.edit form');
+const editsubmit=document.querySelector('.edit .submit');
+const editcancel=document.querySelector('.edit .cancel');
 addbtn.addEventListener('click',()=>{
     dialog.showModal();
 })
@@ -19,6 +24,7 @@ cancelbtn.addEventListener('click',()=>{
         element.classList.remove('selected');
     })
     dialog.close();
+
 })
 
 form.addEventListener('keydown',(event)=>{
@@ -45,7 +51,21 @@ class Book{
     static changeReadStatus(book){
         book.#read=!book.#read;
     }
+    static getTitle(book){
+        return book.#title;
+    }
+    static getAuthor(book){
+        return book.#author;
+    }
+    static getPages(book){
+        return book.#pages;
+    }
 
+    static editbook(book,title,author,pages){
+        book.#title=title;
+        book.#author=author;
+        book.#pages=pages;
+    }
 
 }
 
@@ -55,7 +75,7 @@ document.querySelector('.submit').addEventListener('click',()=>{
     })
 })
 
-document.querySelector('dialog form').addEventListener('submit',(event)=>{
+form.addEventListener('submit',(event)=>{
     event.preventDefault();
     let title=document.querySelector('#title').value;   
     let author=document.querySelector('#author').value;
@@ -66,7 +86,7 @@ document.querySelector('dialog form').addEventListener('submit',(event)=>{
     document.querySelector('#author').value='';
     document.querySelector('#pages').value='';
     document.querySelector('#read').checked=false;
-    document.querySelectorAll('input').forEach((element)=>{
+    document.querySelectorAll('.add input').forEach((element)=>{
         element.classList.remove('selected');
     })
     const div=document.createElement('div');
@@ -112,10 +132,39 @@ document.querySelector('dialog form').addEventListener('submit',(event)=>{
             console.log(mylib);
         }
         else if(event.target.id=='editbtn'){
+            let index=event.target.parentNode.dataset.index;
+            document.querySelector('#edittitle').value=Book.getTitle(mylib[index]);
+            document.querySelector('#editauthor').value=Book.getAuthor(mylib[index]);
+            document.querySelector('#editpages').value=Book.getPages(mylib[index]);
+            currentedit=index;
             editdialog.showModal();
         }
     })
     div.dataset.index=mylib.length-1;
     document.querySelector('.cards').appendChild(div);
     dialog.close();
+})
+
+editcancel.addEventListener('click',()=>{
+    editdialog.close();
+})
+
+editform.addEventListener('keydown',(event)=>{
+    if(event.key==='Enter'){
+        event.preventDefault();
+        editform.requestSubmit();
+    }
+})
+
+editform.addEventListener('submit',(event)=>{
+    event.preventDefault();
+    let newtitle=editform.querySelector('#edittitle').value;
+    let newauthor=editform.querySelector('#editauthor').value;
+    let newpages=editform.querySelector('#editpages').value;
+    Book.editbook(mylib[currentedit],newtitle,newauthor,newpages);
+    const card=document.querySelectorAll('.card')[currentedit];
+    card.querySelectorAll('p')[0].textContent=newtitle;
+    card.querySelectorAll('p')[1].textContent='by '+newauthor;
+    card.querySelectorAll('p')[2].textContent=newpages+' Pages';
+    editdialog.close();
 })
